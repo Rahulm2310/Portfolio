a5a299993924e5927b5a46d074cfb2c03875c82e
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2020-01-05 23:26:57 +0530

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,32 +3,50 @@ const menu = $(".menu");
 const menuNav = $(".menu-nav");
 const menuBranding = $(".menu-branding");
 
+
+
 const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
 
 menuBtn.on("click", toggleMenu);
 
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.addClass("close");
-    menu.addClass("show");
-    menuNav.addClass("show");
-    menuBranding.addClass("show");
-    navItems.forEach(function(item) {
-      $(item).addClass("show");
-    });
 
-    showMenu = true;
-  } else {
-    menuBtn.removeClass("close");
-    menu.removeClass("show");
-    menuNav.removeClass("show");
-    menuBranding.removeClass("show");
-    navItems.forEach(function(item) {
-      $(item).removeClass("show");
-    });
 
-    showMenu = false;
-  }
+function toggleMenu() 
+{
+        if (!showMenu) {
+
+              menuBtn.addClass("close");
+              menu.addClass("show");
+              menuNav.addClass("show");
+              menuBranding.addClass("show");
+
+          
+              navItems.forEach(function(item)
+              {
+                $(item).addClass("show");
+              });
+
+              showMenu = true;
+        } 
+  
+  
+        else {
+          
+              menuBtn.removeClass("close");
+              menu.removeClass("show");
+              menuNav.removeClass("show");
+              menuBranding.removeClass("show");
+          
+          
+              navItems.forEach(function(item)
+              {
+                $(item).removeClass("show");
+              });
+
+              showMenu = false;
+      }
+  
+  
 }
