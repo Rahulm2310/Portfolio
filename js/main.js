b82d233a250a3bf03e544bd0c8a36e6b4e92bc13
Rahulm2310 <rahulmohata2310@gmail.com>
2020-08-03 22:03:56 +0530

    renaming files

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ const projects = {
     title: 'YourMAIT',
     description:
       "YourMAIT is a one stop solution for MAIT (my college) students.It's a web app where students can notes,notices,placement details,upcoming events.There is a feed page where students can create,like,comment posts.Students can also create/join chat rooms",
-    image: 'img/projects/yourMait.JPG',
+    image: 'img/projects/yourMait.jpg',
     url: 'https://yourmait.herokuapp.com',
     technologies: [
       'HTML',
@@ -76,7 +76,7 @@ const projects = {
     title: 'Estylish',
     description:
       "Estylish is a full stack e-commerce platform based on apparels and accessories for men & women. Users can register/login, add products to their cart, process a fake payment using Stripe's payment gateway.",
-    image: 'img/projects/estylish.JPG',
+    image: 'img/projects/estylish.jpg',
     url: 'https://estylish.herokuapp.com',
     technologies: [
       'HTML',
@@ -95,7 +95,7 @@ const projects = {
     title: 'LinkedUp',
     description:
       'LinkedUp is a social networking platform designed for developers around the world to connect with each other. Users can register/login, create, like, comment, delete posts, setup their own profile as well as view other developer profiles.',
-    image: 'img/projects/linkedup.JPG',
+    image: 'img/projects/linkedup.jpg',
     url: 'https://linked-up-net.herokuapp.com',
     technologies: [
       'HTML',
@@ -114,7 +114,7 @@ const projects = {
     title: 'India fights Corona',
     description:
       'India fights Corona is a Covid-19 tracker application to track current situation of novel coronavirus in India. It shows state wise as well as district wise data.It also has a news section and a helplines section with covid-19 helplines for all states.',
-    image: 'img/projects/corona.JPG',
+    image: 'img/projects/corona.jpg',
     url: 'https://india-fights-corona.herokuapp.com',
     technologies: [
       'HTML',
@@ -132,7 +132,7 @@ const projects = {
     title: 'Swizzle',
     description:
       "Swizzle is a men's shirt manufacturing company based in New Delhi, India. They are a best quality shirt manufacturer providing one stop solution for all bulk needs.",
-    image: 'img/projects/swizzle.JPG',
+    image: 'img/projects/swizzle.jpg',
     url: 'https://swizzleshirts.herokuapp.com',
     technologies: [
       'HTML',
